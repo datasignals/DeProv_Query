6c4b7a443db3c385f2d09e5b80442138f40a4f8c
Nikita Gugale <nikitagu1998@gmail.com>
2024-08-05 11:54:05 +0530

    "querry changes 2"

--- src/QuerryByAddress.ts
+++ src/QuerryByAddress.ts
@@ -32,50 +32,38 @@ export default class QueryAllByAddress{
                     const { templateModule } = api.query; // Adjust to match your pallet name in Substrate
                     console.log("QUERRY",templateModule)
                     // Querying the storage
-                    const storageResult = await templateModule.disReAssembly(account);
-                    // const entries = await api.query.templateModule.disReAssembly.entries();
-                    // console.log("DISASSEMBLY",entries);
-                    // let creationtimeHex =  '0x6c6f636b756c6172000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000';
-                    // const creationtime = await this.decodeHex(creationtimeHex);
-                    // console.log("Decoded Creation Time:", creationtime);
-
-                    const data = storageResult.toJSON() as {
-                        creationtime: string;
-                        filepath: string;
-                        eventkey: string;
-                    };
-                    const decodedData = this.decodeData(data);
-                    console.log("DATA",decodedData)
-                    return decodedData;
-                    // Query all entries in the storage map
-                // const entries = await api.query.templateModule.disReAssembly.entries();
-                // // console.log("ENTRIES", entries.());
-
-                // // Fetch and decode all records
-                // const allRecords = entries.map(([storageKey, storageValue]) => {
-                //     console.log("STOR",storageKey)
-                //     console.log("STOR",storageValue.toHuman)
-                //     const accountId = storageKey.args.map(arg => arg.toString()).join('');
-                //     // const decodedValue = storageValue.unwrapOr(null);
-
-                //     // const decodedKey = key.toHuman() as string;
-                //     console.log("KEYS",accountId);
-                //     // console.log("VALUE",value.toHex);
-                //     // const decodedValue = value.toJSON() as {
-                //     //     creationtime: string;
-                //     //     filepath: string;
-                //     //     eventkey: string;
-                //     // };
-                //     // return {
-                //     //     key: decodedKey,
-                //     //     value: this.decodeData(decodedValue)
-                //     // };
-                   
-                // });
-
-                // return allRecords;
-                    
-                
+                    const entries = await templateModule.disReAssembly.entries();
+                    // console.log("ENTRIES", entries);
+                    // Decode and log the entries
+                    const allRecords = await Promise.all(entries.map(async ([storageKey, storageValue]) => {
+                        // Decode the storage key
+                        const accountId = storageKey.args.map(arg => arg.toString()).join('');
+                      
+                        // Convert storage value to a human-readable format
+                        const decodedValue = storageValue.toHuman();
+                      
+                        // Log the decoded key and value
+                        console.log("KEYS", accountId);
+                        console.log("VALUE", decodedValue);
+                      
+                        const data = storageValue.toJSON() as {
+                          eventtype: string;
+                          creationtime: string;
+                          filepath: string;
+                          eventkey: string;
+                        };
+                      
+                        // Await the result of decodeData
+                        const decodedData = await this.decodeData(data);
+                        console.log("DATA", decodedData);
+                      
+                        return {
+                          key: accountId,
+                          value: decodedData
+                        };
+                      }));
+                      
+                      return allRecords;
                 
                 }
             } else {
@@ -90,8 +78,9 @@ export default class QueryAllByAddress{
             }
         }
     }
-    async decodeData(data: { creationtime: string; filepath: string; eventkey: string }): Promise<{ creationtime: string; filepath: string; eventkey: string; }> {
+    async decodeData(data: { eventtype: string,creationtime: string; filepath: string; eventkey: string }): Promise<{ eventtype: string;creationtime: string; filepath: string; eventkey: string; }> {
         return {
+            eventtype: this.decodeHex(data.eventtype),
             creationtime: this.decodeHex(data.creationtime),
             filepath: this.decodeHex(data.filepath),
             eventkey: this.decodeHex(data.eventkey)
