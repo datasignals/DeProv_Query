19a2ff1173fc8cf6f62540c226bdc8372584d1b6
Nikita Gugale <nikitagu1998@gmail.com>
2024-08-13 13:56:03 +0530

    "cors resolved"

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ import bodyParser from "body-parser";
 // // import {MongoService} from "./MongoService";
 import {Route} from "./Routes";
 // import * as config from '../config/config.json';
-
+import cors from 'cors';
 const app = Express()
+app.use(cors());
 //Port
 const port = 3005;
 // app.use(Cors({credentials: true}));
