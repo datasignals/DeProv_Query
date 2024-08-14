3f2fcd0d6d4ec57ce7602dbcb03af094c3f636d2
Nikita Gugale <nikitagu1998@gmail.com>
2024-08-14 14:37:56 +0530

    "cors resolved 2"

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 
-// import Cors from "cors";
+
 import Express from "express";
 import bodyParser from "body-parser";
 // // import {MongoService} from "./MongoService";
 import {Route} from "./Routes";
 // import * as config from '../config/config.json';
-import cors from 'cors';
+// import cors from 'cors';
+import Cors from "cors";
 const app = Express()
-app.use(cors());
 //Port
 const port = 3005;
-// app.use(Cors({credentials: true}));
+
+app.use(Cors({credentials: true}));
 app.use(bodyParser.json());
 
 app.get('/events/accountId',Route.Get.QuerryAllByAddress);
@@ -18,6 +19,4 @@ app.get('/events/filerecords',Route.Get.QuerryAllByFile);
 
 app.listen(port, () => console.log(`File System Deprov Query listening on port ${port}`));
 
-function Cors(arg0: { credentials: boolean; }): any {
-    throw new Error("Function not implemented.");
-}
+
